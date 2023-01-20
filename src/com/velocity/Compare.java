4464a574a5d20ca181225f7cf56ad191a352d421
Padmapriya <padmapriyajyoti99@gmail.com>
2023-01-20 23:32:30 +0530

    campared to arrays

---
+++ src/com/velocity/Compare.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class Compare {
+
+	public static void main(String[] args) {
+		int [] a = {2,3,4,5};
+		int [] b = {3,4,5,6};
+		if (a.length == b.length) {
+			System.out.println("Length is equal");
+		} else {
+			System.out.println("Length is not equal");
+		}
+	}
+}
